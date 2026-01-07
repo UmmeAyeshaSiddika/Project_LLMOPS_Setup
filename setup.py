616c6d8a041a192ffdb5c4a034c162588bcc64b5
UmmeAyeshaSiddika <133027375+UmmeAyeshaSiddika@users.noreply.github.com>
2026-01-07 11:43:05 +0900

    init file initialize

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="Project_LLMOPS_Setup",
+    version="0.1",
+    author="Umme Ayesha Siddika",
+    packages="find_packages()",
+   # install_requires=[
+   #    "fastapi",
+   #     "uvicorn",
+   #    "python-dotenv",
+   #],
+)
